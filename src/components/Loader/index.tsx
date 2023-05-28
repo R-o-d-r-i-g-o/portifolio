dbad0e284cab44ac6f8d846c7c62f5daa365cc60
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-05-28 20:03:07 -0300

    feat: add loader styles

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,21 +1,17 @@
+"use client"
+
 import { Html, useProgress } from "@react-three/drei";
+import * as S from './styles';
 
 const Loader = () => {
   const { progress } = useProgress();
 
   return (
     <Html>
-      <span className="canvas-load"></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: "#f1f1f1",
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
+      <S.CanvasLoad></S.CanvasLoad>
+      <S.TextLoad>
         {progress.toFixed(0)}%
-      </p>
+      </S.TextLoad>
     </Html>
   );
 };
