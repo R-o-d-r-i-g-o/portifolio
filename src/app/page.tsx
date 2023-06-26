ffe27b6daae98f623e3c20b6ebb39235fb6d2dbc
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-06-25 22:08:00 -0300

    feat: change presentation texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,10 +26,11 @@ const Home = () => {
         isBodyText
         subTitle='Education'
         bodyText={
-          'Rodrigo is a full-cycle software engineer and cloud associate based in Brazil, with a passion for '    +
-          'building scalable and reliable systems. He possesses a strong background in developing, maintaining, ' +
-          'and deploying payment systems and programs. Currently, his focus lies in exploring cloud solutions  '  +
-          'and their practical applications in solving real-life problems.'
+          'One and a half years before securing his first job as a programmer, Rodrigo embarked on his software '   +
+          'engineering journey by enrolling in the esteemed software engineering course at Uni-FACEF. Since then, ' + 
+          'he has diligently pursued practical experience through independent studies, separate from his academic ' + 
+          'and professional commitments. - For more details, explore the certifications section on my linkedin '    +
+          'profile.'
         }
       />
       <Skills />
