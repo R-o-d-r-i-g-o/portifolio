bd00acc9ff779c11917229e5f93d67ee0656916f
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-08-06 21:58:15 -0300

    fix: remove momently metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,38 +17,38 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const baseFont = Quicksand({ subsets: ['latin'] })
 
-const metadata = {
-  icon: {
-    rel: 'icon',
-    sizes: '32x32',
-    url: '/favicon.ico'
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      follow: true,
-      nocache: false
-    }
-  },
-  title: {
-    default: 'Rodrigo',
-    template: 'Rodrigo - %s'
-  },
-  keywords: [
-    'Rodrigo designs',
-    'Rodrigo developer',
-    'Rodrigo portfolio',
-    'Rodrigo Marques Ribeiro',
-    'Rodrigo M. Ribeiro'
-  ],
-  description:
-    "Welcome to my portfolio site! I'm Rodrigo, a passionate and skilled developer specializing " +
-    "manly in front-ent development. On this site, you'll find a showcase of my work, including " +
-    'various projects and contributions I have made throughout my career.'
-}
+// const metadata = {
+//   icon: {
+//     rel: 'icon',
+//     sizes: '32x32',
+//     url: '/favicon.ico'
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     nocache: false,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       nocache: false
+//     }
+//   },
+//   title: {
+//     default: 'Rodrigo',
+//     template: 'Rodrigo - %s'
+//   },
+//   keywords: [
+//     'Rodrigo designs',
+//     'Rodrigo developer',
+//     'Rodrigo portfolio',
+//     'Rodrigo Marques Ribeiro',
+//     'Rodrigo M. Ribeiro'
+//   ],
+//   description:
+//     "Welcome to my portfolio site! I'm Rodrigo, a passionate and skilled developer specializing " +
+//     "manly in front-ent development. On this site, you'll find a showcase of my work, including " +
+//     'various projects and contributions I have made throughout my career.'
+// }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const [theme] = useLocalStorage('theme', defaultTheme)
@@ -78,4 +78,4 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default appWithTranslation(RootLayout as any)
-export { metadata }
+// export { metadata }
