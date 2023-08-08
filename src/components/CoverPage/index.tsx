2860031ccd869c7a5d74889b85a590b392025c76
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-08-08 19:33:21 -0300

    fix: remove params

--- src/components/CoverPage/index.tsx
+++ src/components/CoverPage/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from 'react'
-import { useSearchParams } from 'next/navigation'
 import * as S from './styles'
 
 type Props = {
@@ -9,17 +8,11 @@ type Props = {
 }
 
 const Works = ({ lottie }: Props) => {
-  const searchParams = useSearchParams()
-  const lang = searchParams.get('lang')
-
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: lottie
   }
-
-  // eslint-disable-next-line no-console
-  console.log(lang)
 
   return (
     <S.Container>
